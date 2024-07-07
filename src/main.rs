57a0238ea8bf6ef242e6db1317cbb7cb4a31e72e
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-07 21:02:38 +0100

    try display drawing

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,32 @@
 #![no_main]
 #![allow(async_fn_in_trait)]
 
+use core::cell::RefCell;
 use core::str;
 
 use cyw43_pio::PioSpi;
 use defmt::*;
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_net::Stack;
-use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::spi::{self, Spi};
+use embassy_rp::spi::{Blocking, Phase, Polarity};
+use embassy_rp::{bind_interrupts, config};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::blocking_mutex::Mutex;
+use embassy_time::Delay;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::geometry::Point;
+use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::pixelcolor::{Rgb565, RgbColor};
+use embedded_graphics::primitives::{Primitive, Rectangle};
+use embedded_graphics::text::Text;
+use embedded_graphics::Drawable;
+use st7735_lcd::{Orientation, ST7735};
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -38,47 +54,51 @@ async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    info!("Hello World!");
-
     let p = embassy_rp::init(Default::default());
 
-    let fw = include_bytes!("../firmware/43439A0.bin");
-    let clm = include_bytes!("../firmware/43439A0_clm.bin");
+    let clk = p.PIN_18;
+    let mosi = p.PIN_19;
+    let miso = p.PIN_16;
+    let display_cs = p.PIN_20;
+    let dcx = p.PIN_22;
+    let rst = p.PIN_26;
+    let bl = p.PIN_17;
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    let mut display_config = spi::Config::default();
+    display_config.frequency = 64_000_000;
+    display_config.phase = Phase::CaptureOnSecondTransition;
+    display_config.polarity = Polarity::IdleHigh;
 
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(
-        &mut pio.common,
-        pio.sm0,
-        pio.irq0,
-        cs,
-        p.PIN_24,
-        p.PIN_29,
-        p.DMA_CH0,
+    let spi: Spi<'_, _, Blocking> =
+        Spi::new_blocking(p.SPI0, clk, mosi, miso, display_config.clone());
+    let spi_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi));
+
+    let display_spi = SpiDeviceWithConfig::new(
+        &spi_bus,
+        Output::new(display_cs, Level::Low),
+        display_config,
     );
 
-    static STATE: StaticCell<cyw43::State> = StaticCell::new();
-    let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-    unwrap!(spawner.spawn(wifi_task(runner)));
+    let dcx = Output::new(dcx, Level::Low);
+    let rst = Output::new(rst, Level::Low);
 
-    control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
+    let _bl = Output::new(bl, Level::High);
 
-    let mut scanner = control.scan().await;
-    while let Some(bss) = scanner.next().await {
-        if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
-            info!("scanned {} == {:x}", ssid_str, bss.bssid);
-        }
-    }
+    let mut disp = ST7735::new(display_spi, dcx, rst, true, false, 160, 128);
+
+    disp.init(&mut Delay).unwrap();
+    disp.set_orientation(&Orientation::Landscape).unwrap();
+    disp.clear(Rgb565::BLACK).unwrap();
+    disp.set_offset(0, 25);
+
+    let style = MonoTextStyle::new(&FONT_10X20, Rgb565::GREEN);
+    Text::new(
+        "Hello embedded_graphics \n + embassy + RP2040!",
+        Point::new(20, 200),
+        style,
+    )
+    .draw(&mut disp)
+    .unwrap();
+
+    loop {}
 }
