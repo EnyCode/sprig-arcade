b1fd4bf1db241d2dd94d15c93695d8b41ee9f628
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-11 18:41:32 +0100

    BUTTONS (and better tooling)

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,105 @@ bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => usb::InterruptHandler<peripherals::USB>;
 });
 
+// we import everything here to avoid repeats and for ease of use
+// makes it easier to eventually move to a fixed memory location if their all together (probably)
+const ARCADE_LOGO: &'static [u8; 2347] = include_bytes!("assets/arcade.tga");
+const BUTTONS: &'static [u8; 1942] = include_bytes!("assets/buttons.tga");
+const BTN: &'static [u8; 204] = include_bytes!("assets/btn.tga");
+const SELECTED_BTN: &'static [u8; 204] = include_bytes!("assets/selected_btn.tga");
+
+const HOME_ICON: &'static [u8; 190] = include_bytes!("assets/buttons/home.tga");
+const SESSION_ICON: &'static [u8; 232] = include_bytes!("assets/buttons/session.tga");
+const LEADERBOARD_ICON: &'static [u8; 160] = include_bytes!("assets/buttons/leaderboard.tga");
+const PROJECTS_ICON: &'static [u8; 182] = include_bytes!("assets/buttons/projects.tga");
+const WISHLIST_ICON: &'static [u8; 218] = include_bytes!("assets/buttons/wishlist.tga");
+const SHOP_ICON: &'static [u8; 204] = include_bytes!("assets/buttons/shop.tga");
+const ERRORS_ICON: &'static [u8; 212] = include_bytes!("assets/buttons/errors.tga");
+
+const HOME_SELECTED_ICON: &'static [u8; 190] = include_bytes!("assets/buttons/home_selected.tga");
+const SESSION_SELECTED_ICON: &'static [u8; 232] =
+    include_bytes!("assets/buttons/session_selected.tga");
+const LEADERBOARD_SELECTED_ICON: &'static [u8; 160] =
+    include_bytes!("assets/buttons/leaderboard_selected.tga");
+const PROJECTS_SELECTED_ICON: &'static [u8; 182] =
+    include_bytes!("assets/buttons/projects_selected.tga");
+const WISHLIST_SELECTED_ICON: &'static [u8; 218] =
+    include_bytes!("assets/buttons/wishlist_selected.tga");
+const SHOP_SELECTED_ICON: &'static [u8; 204] = include_bytes!("assets/buttons/shop_selected.tga");
+const ERRORS_SELECTED_ICON: &'static [u8; 212] =
+    include_bytes!("assets/buttons/errors_selected.tga");
+
 #[embassy_executor::task]
 async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
+
+pub enum NavButton {
+    None,
+    Home,
+    Session,
+    Leaderboard,
+    Projects,
+    Wishlist, // TODO: merge wishlist with shop and make a settings page?
+    Shop,
+    // TODO: hide this by default
+    Errors,
+}
+
+impl NavButton {
+    pub fn pos(&self) -> Point {
+        match self {
+            NavButton::Home => Point::new(23, 0),
+            NavButton::Session => Point::new(39, 0),
+            NavButton::Leaderboard => Point::new(55, 0),
+            NavButton::Projects => Point::new(71, 0),
+            NavButton::Wishlist => Point::new(87, 0),
+            NavButton::Shop => Point::new(103, 0),
+            NavButton::Errors => Point::new(119, 0),
+            NavButton::None => Point::new(0, 0),
+        }
+    }
+
+    pub fn icon(&self) -> Tga<Rgb565> {
+        Tga::from_slice(match self {
+            NavButton::Home => HOME_ICON,
+            NavButton::Session => SESSION_ICON,
+            NavButton::Leaderboard => LEADERBOARD_ICON,
+            NavButton::Projects => PROJECTS_ICON,
+            NavButton::Wishlist => WISHLIST_ICON,
+            NavButton::Shop => SHOP_ICON,
+            NavButton::Errors => ERRORS_ICON,
+            NavButton::None => HOME_ICON,
+        })
+        .unwrap()
+    }
+
+    pub fn selected_icon(&self) -> Tga<Rgb565> {
+        Tga::from_slice(match self {
+            NavButton::Home => HOME_SELECTED_ICON,
+            NavButton::Session => SESSION_SELECTED_ICON,
+            NavButton::Leaderboard => LEADERBOARD_SELECTED_ICON,
+            NavButton::Projects => PROJECTS_SELECTED_ICON,
+            NavButton::Wishlist => WISHLIST_SELECTED_ICON,
+            NavButton::Shop => SHOP_SELECTED_ICON,
+            NavButton::Errors => ERRORS_SELECTED_ICON,
+            NavButton::None => HOME_SELECTED_ICON,
+        })
+        .unwrap()
+    }
+
+    pub fn icon_pos(&self) -> Point {
+        match self {
+            NavButton::Home => Point::new(28, 2),
+            NavButton::Session => Point::new(43, 1),
+            NavButton::Leaderboard => Point::new(55, 0),
+            NavButton::Projects => Point::new(71, 0),
+            NavButton::Wishlist => Point::new(87, 0),
+            NavButton::Shop => Point::new(103, 0),
+            NavButton::Errors => Point::new(119, 0),
+            NavButton::None => Point::new(0, 0),
+        }
+    }
 }
 
 #[embassy_executor::main]
@@ -51,13 +147,13 @@ async fn main(spawner: Spawner) {
         Timer::after_secs(1).await;
     }
     info!("Launched Arcade Sprig!");
+    // TODO: remove logging, dont know why its needed but it works
     Timer::after_nanos(20000).await;
 
     let clk = p.PIN_18;
     let mosi = p.PIN_19;
     let miso = p.PIN_16;
     let display_cs = p.PIN_20;
-    //let cs = p.PIN_20;
     let dcx = p.PIN_22;
     let rst = p.PIN_26;
     let bl = p.PIN_17;
@@ -79,7 +175,6 @@ async fn main(spawner: Spawner) {
 
     let dcx = Output::new(dcx, Level::Low);
     let rst = Output::new(rst, Level::Low);
-    //let cs = Output::new(cs, Level::Low);
 
     let _bl = Output::new(bl, Level::High);
 
@@ -89,27 +184,35 @@ async fn main(spawner: Spawner) {
     disp.set_orientation(&Orientation::Landscape).unwrap();
     disp.clear(Rgb565::new(31, 59, 26)).unwrap();
 
-    let tga: Tga<Rgb565> = Tga::from_slice(include_bytes!("assets/arcade.tga")).unwrap();
+    // BOILERPLATE MARK
 
-    Image::new(&tga, Point::new(30, 98))
+    let logo: Tga<Rgb565> = Tga::from_slice(ARCADE_LOGO).unwrap();
+    let buttons: Tga<Rgb565> = Tga::from_slice(BUTTONS).unwrap();
+    let btn: Tga<Rgb565> = Tga::from_slice(BTN).unwrap();
+    let selected_btn: Tga<Rgb565> = Tga::from_slice(SELECTED_BTN).unwrap();
+
+    Image::new(&logo, Point::new(30, 98))
         .draw(&mut disp)
         .unwrap();
 
-    Rectangle::new(Point::new(0, 0), Size::new(20, 20))
-        .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 2))
+    Image::new(&buttons, Point::new(23, 0))
         .draw(&mut disp)
         .unwrap();
 
-    //disp.set_offset(0, 25);
+    let active = NavButton::Home;
+    let selected = NavButton::Session;
 
-    //let style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
-    //Text::new(
-    //    "Hello embedded_graphics \n + embassy + RP2040!",
-    //    Point::new(20, 200),
-    //    style,
-    //)
-    //.draw(&mut disp)
-    //.unwrap();
+    Image::new(&selected_btn, selected.pos())
+        .draw(&mut disp)
+        .unwrap();
+
+    Image::new(&selected.selected_icon(), selected.icon_pos())
+        .draw(&mut disp)
+        .unwrap();
+
+    info!("Done!");
+    // TODO: remove logging, dont know why its needed but it works
+    Timer::after_nanos(20000).await;
 
     loop {}
 }
